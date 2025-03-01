9bb8cd5982a14609f50fa5dcbd43ffc04ba37a7a
Connor Rhodes <connor@rhodes.contact>
2025-03-01 18:39:40 +0000

    generate ssh key

--- ubuntu.rb
+++ ubuntu.rb
@@ -10,7 +10,12 @@ packages = %w[
   lf
 ]
 
-`sudo apt install -y #{packages.join(' ')}`
+`sudo apt-get update && sudo apt-get upgrade -y`
+`sudo apt-get install -y #{packages.join(' ')}`
 
 # `sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply 0xConnorRhodes`
 # `~/.local/bin/chezmoi apply` # second run to decrypt secrets
+
+unless File.exist?(File.expand_path("~/.ssh/id_ed25519"))
+  `ssh-keygen -q -a 100 -t ed25519 -f "$HOME/.ssh/id_ed25519" -N ""`
+end
